13c6c4e00a7f228ec12eec0c1279f0733622853c
RokiWielki <Cruck222@interia.pl>
2022-11-26 10:51:52 +0100

    Dodaj pliki projektów.

---
+++ Operatory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TautologySpoj
+{
+    internal class Operatory
+    {
+        public static void C()
+        {
+            Console.WriteLine(" i ");
+        }
+        public void D()
+        {
+            Console.WriteLine(" Lub ");
+        }
+        public void I()
+        {
+            Console.WriteLine(" Implementacja ");
+        }
+        public void E()
+        {
+            Console.WriteLine(" Wtedy i tylko wtedy ");
+        }
+        public void N()
+        {
+            Console.WriteLine(" Nie ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TautologySpoj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            Operatory.C();
+        }
+
+        public static void Operator()
+        {
+
+        }
+        public static void Zmienne()
+        {
+            
+        }
+    }
+
+}
